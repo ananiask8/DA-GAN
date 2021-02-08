38900df3913a86837b432affc65ce6505960f8b1
Jose Ananias Hilario Reyes <j.ananias.hilario@gmail.com>
2021-02-08 12:49:34 +0100

    ip update

--- src/main.py
+++ src/main.py
@@ -10,7 +10,10 @@ from trainer import Trainer
 torch.manual_seed(args.seed)
 checkpoint = utility.checkpoint(args)
 
-
+"""
+python main.py --save_results --save_gt --save_models --dir_data ../../datasets/ 
+               --data_train CASPEALR1 --data_test CASPEALR1
+"""
 if checkpoint.ok:
     loader = data.Data(args)
     model = model.Model(args, checkpoint)
